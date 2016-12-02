dd7421189ae8fdc74dd9242dff4746e50ee5b857
arivin <btc022003@gmail.com>
2016-12-02 17:42:34 +0800

    修改演员详情的展示样式

--- src/components/CastDetail.vue
+++ src/components/CastDetail.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        演员详情:
-        <p v-for="k in detail">{{k}}</p>
+        <img :src="detail.avatars.large" class="detail-avatar" alt="">
+        <h3>{{detail.name}}</h3>
+        <ul>
+            <li v-for="item in detail.works" :title="item.subject.title">{{item.subject.title | doSub}}</li>
+        </ul>
     </div>
 </template>
 <script>
@@ -9,7 +12,7 @@
     export default{
         data(){
             return{
-                detail:{}
+                detail:{avatars:{}}
             }
         },
         created(){
@@ -21,11 +24,25 @@
         },
         methods:{
             fetchData(){
-                this.detail = {}
+                this.detail = {avatars:{}}
                 var app =this
                 $.getJSON('http://api.douban.com/v2/movie/celebrity/'+this.$route.params.id+'?callback=?')
                     .then(res=>app.detail=res)
             }
+        },
+        filters:{
+            doSub(val){
+                return val.length>8?val.substring(0,7)+"...":val
+            }
         }
     }
 </script>
+<style>
+.detail-avatar{
+    width:120px;
+    max-height:300px;
+}
+ul li{
+    text-align:left;
+}
+</style>
